bf920082110700ef95fe810b20cb9ffc98a5e254
Luiz da Mata <luizrdamata@gmail.com>
2026-07-06 09:30:23 -0300

    feat(style): improve button animation and variants

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,9 +4,17 @@ export const Home = () => {
   return (
     <>
       <div className="bg-purple p-20 text-white text-4xl">Rift9</div>{" "}
-      <div className="mt-5 flex items-center justify-center">
+      <div className="mt-5 flex p-5 items-center justify-center gap-5">
         <Button
           title="Daily"
+          variant="default"
+          onClick={() => {
+            console.log("Entrando...");
+          }}
+        />
+        <Button
+          title="Exit"
+          variant="destructive"
           onClick={() => {
             console.log("Entrando...");
           }}
